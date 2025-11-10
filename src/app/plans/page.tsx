c93ec818b475560488ac763b6d7f2d6e80fdb0ae
Mustafa Asif <mustafaasif0506@gmail.com>
2025-11-10 10:46:23 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/componentsui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Check } from 'lucide-react';
 
 const plans = [
